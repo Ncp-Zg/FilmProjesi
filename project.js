cff6eb0c6cdddd0ca0dd8a36f998e4bc02d93184
Necip <ncptzgn@gmail.com>
2021-02-27 10:51:41 +0300

    Film Projesi

---
+++ project.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI Objesini Başlatma
+const ui = new UI();
+
+// Tüm eventleri yükleme
+
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener("submit",addFilm);
+}
+function addFilm(e){
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if (title === "" || director === "" || url === ""){
+
+    }
+    else{
+        // Yeni Film
+        const newFilm = new Film(title,director,url);
+
+        ui.addFilmToUI (newFilm); // Arayüze Film Ekleme
+    }
+
+
+ui.clearInputs(titleElement,urlElement,directorElement);
+
+    e.preventDefault();
+}
